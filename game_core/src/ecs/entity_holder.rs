ed9e813cf1594dad8fdd57e636e98987a1093735
Zach Amsler <zachary.amsler@citizensbank.com>
2026-08-25 13:59:27 -0500

    Fix entity_holder bugs

--- game_core/src/ecs/entity_holder.rs
+++ game_core/src/ecs/entity_holder.rs
@@ -19,8 +19,12 @@ impl EntityHolder {
     }
 
     pub fn delete_entity(&mut self, entity: Entity) {
-        self.generations[entity.index as usize] += 1;
-        self.free.push(entity.index);
+        if let Some(generation) = self.generations.get_mut(entity.index as usize) {
+            if *generation == entity.generation {
+                *generation += 1;
+                self.free.push(entity.index);
+            }
+        }
     }
 
     pub fn is_valid(&self, entity: Entity) -> bool {
@@ -65,8 +69,52 @@ mod tests {
         assert!(!holder.is_valid(entity))
     }
 
-    // #[test]
-    // fn remove_entity() {
-    //     assert!(false)
-    // }
+    #[test]
+    fn delete_entity_make_not_valid() {
+        let mut holder = EntityHolder::default();
+        let entity = holder.create_entity();
+        holder.delete_entity(entity);
+        assert!(!holder.is_valid(entity))
+    }
+
+    #[test]
+    fn delete_entity_do_nothing_entity_not_exist() {
+        let mut holder = EntityHolder::default();
+        let entity = holder.create_entity();
+        let entity2 = Entity::new(1, 0);
+        holder.delete_entity(entity2);
+        assert!(holder.is_valid(entity))
+    }
+
+    #[test]
+    fn create_entity_reuse_slot() {
+        let mut holder = EntityHolder::default();
+        let entity = holder.create_entity();
+        let entity2 = holder.create_entity();
+        holder.delete_entity(entity);
+        let entity = holder.create_entity();
+        assert!(holder.is_valid(entity));
+        assert!(holder.is_valid(entity2));
+        assert_eq!(entity.generation, 1);
+        assert_eq!(entity.index, 0);
+    }
+
+    #[test]
+    fn old_generation_not_remove_entity() {
+        let mut holder = EntityHolder::default();
+        let entity = holder.create_entity();
+        holder.delete_entity(entity);
+        let entity2 = holder.create_entity();
+        holder.delete_entity(entity);
+        assert!(holder.is_valid(entity2));
+    }
+
+    #[test]
+    fn delete_entity_twice() {
+        let mut holder = EntityHolder::default();
+        let entity = holder.create_entity();
+        holder.delete_entity(entity);
+        holder.delete_entity(entity);
+        assert!(!holder.is_valid(entity))
+    }
 }
