fb493afb72d5098b2ade27b01ebcf9d832371db8
Zach Amsler <zachary.amsler@citizensbank.com>
2026-08-31 17:18:42 -0500

    Update chunk renderer to render lines

--- game_app/src/chunk_renderer.rs
+++ game_app/src/chunk_renderer.rs
@@ -1,5 +1,6 @@
 use crate::entity_renderers::EntityRenderer;
 use game_logic::Chunk;
+use macroquad::{color::WHITE, shapes::draw_rectangle_lines, text::draw_text};
 
 /// Renders every entity in a chunk.
 pub struct ChunkRenderer {
@@ -14,8 +15,22 @@ impl ChunkRenderer {
     }
 
     pub fn render(&self, chunk: &Chunk) {
-        for entity in &chunk.entities {
-            // self.entity_renderer.render(entity);
+        let w = 32.0;
+        let x = chunk.coords.x;
+        let y = chunk.coords.y;
+        let s = format!("({x},{y})");
+        draw_text(&s, x as f32, y as f32 + 10.0, 16.0, WHITE);
+        for i in 0..8 {
+            for j in 0..8 {
+                draw_rectangle_lines(
+                    chunk.coords.x as f32 + (i as f32 * w),
+                    chunk.coords.y as f32 + (j as f32 * w),
+                    w,
+                    w,
+                    1.0,
+                    WHITE,
+                );
+            }
         }
     }
 }
