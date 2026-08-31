83b4f5c94147b92973db02995d886bc20ad694af
Zach Amsler <zachary.amsler@citizensbank.com>
2026-08-31 16:44:01 -0500

    Add camera dragging

--- game_app/src/main.rs
+++ game_app/src/main.rs
@@ -28,6 +28,11 @@ async fn main() {
     //     position: game_logic::Position { x: 50, y: 50 },
     // });
 
+    let mut prev_mouse_position_x: Option<f32> = None;
+    let mut prev_mouse_position_y: Option<f32> = None;
+    let mut camera_position_x = 0.0;
+    let mut camera_position_y = 0.0;
+
     loop {
         clear_background(BLACK);
         chunk_renderer.render(&chunk);
@@ -35,7 +40,10 @@ async fn main() {
         let view_w = VIEW_H * screen_width() / screen_height();
 
         set_camera(&Camera2D::from_display_rect(Rect::new(
-            15.0, 15.0, view_w, VIEW_H,
+            camera_position_x,
+            camera_position_y,
+            view_w,
+            VIEW_H,
         )));
 
         let w = 15.0;
@@ -59,6 +67,24 @@ async fn main() {
             log::debug!("Mouse clicked at ({x}, {y})");
             log::debug!("Grid location ({grid_x}, {grid_y})");
             log::debug!("Chunk coord ({chunk_x}, {chunk_y})");
+        }
+
+        if is_mouse_button_down(MouseButton::Left) {
+            let (x, y) = mouse_position();
+            if let Some(prev_x) = prev_mouse_position_x
+                && let Some(prev_y) = prev_mouse_position_y
+            {
+                let diff_x = prev_x - x;
+                let diff_y = prev_y - y;
+
+                camera_position_x += diff_x;
+                camera_position_y -= diff_y;
+            }
+            prev_mouse_position_x = Some(x);
+            prev_mouse_position_y = Some(y);
+        } else {
+            prev_mouse_position_x = None;
+            prev_mouse_position_y = None;
         }
 
         // draw_rectangle_lines(100.0, 100.0, 32.0, 32.0, 2.0, WHITE);
